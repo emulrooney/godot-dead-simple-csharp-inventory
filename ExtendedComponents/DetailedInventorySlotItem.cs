b2d236a6a70ac23eb500ed3248e6252688ebc220
emulrooney <evanmulrooney@gmail.com>
2024-08-19 19:04:04 -0400

    Lil fix for dragging stuff

---
+++ ExtendedComponents/DetailedInventorySlotItem.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+/// <summary>
+/// Variation on <see cref="InventorySlotItem"/> with <see cref="Description"/>.
+/// </summary>
+public partial class DetailedInventorySlotItem : InventorySlotItem
+{
+    /// <summary>
+    /// Extra text to show.
+    /// </summary>
+    [Export(PropertyHint.MultilineText)] public string Description { get; private set; }
+}
